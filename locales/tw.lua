28ac52d089c2a8f0ae1eb3d9f6f949e58a30e40d
AiReiKe <78699906+AiReiKe@users.noreply.github.com>
2022-01-21 14:22:41 +0800

    Update tw.lua

--- locales/tw.lua
+++ locales/tw.lua
@@ -5,5 +5,7 @@ Locales['tw'] = {
     ['nearby_players'] = "給予附近玩家",
     ['no_player_nearby'] = "附近沒有玩家",
     ["get_key_from"] = "~y~%s ~s~給了你 ~g~%s~s~ 的鑰匙",
-    ["refreshed_key"] = "已刷新車鑰匙"
+    ["refreshed_key"] = "已刷新車鑰匙",
+    ["carkey_command"] = "列出擁有的車鑰匙(可將已有車鑰匙借別人)",
+    ["refresh_command"] = "刷新車鑰匙"
 }
