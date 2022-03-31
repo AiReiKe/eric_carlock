c16cbbed97c0b44151e5496aef77cf817fbae0a0
AiReiKe <78699906+AiReiKe@users.noreply.github.com>
2022-03-31 23:17:54 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,7 +3,8 @@ ESX = nil
 TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
 
 ESX.RegisterServerCallback("eric_carlock:getOwnedVehicle", function(source, cb)
-    local identifier = GetPlayerIdentifier(source, 0)
+	local xPlayer = ESX.GetPlayerFromId(source)
+    	local identifier = xPlayer.identifier
 
 	MySQL.Async.fetchAll('SELECT plate FROM owned_vehicles WHERE owner = @owner', {
 		['@owner'] = identifier
@@ -22,7 +23,7 @@ end)
 RegisterNetEvent("eric_carlock:givecarkey")
 AddEventHandler("eric_carlock:givecarkey", function(player, plate)
 	local _source = source
-    TriggerClientEvent("eric_carlock:addcarkey", player, plate)
+    	TriggerClientEvent("eric_carlock:addcarkey", player, plate)
 	TriggerClientEvent("esx:showNotification", player, _U("get_key_from", GetPlayerName(_source), plate))
 end)
 
